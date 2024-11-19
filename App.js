f989576eb6c65be33891af639902664773904af7
usmanshafique7 <s7.usman@gmail.com>
2024-11-19 18:02:37 +0500

    test

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+      </header>
+    </div>
+  );
+}
+export default App;
