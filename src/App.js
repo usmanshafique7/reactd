e2c31eefa6682e7d944b88541e68a414ffeadb2f
usmanshafique7 <s7.usman@gmail.com>
2024-11-21 15:26:12 +0500

    Test push

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Not welcome to my react app test this
+          Not welcome to my react app
         </p>
         <a
           className="App-link"
