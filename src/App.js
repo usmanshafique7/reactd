afe93a6e1e1920078294bf49a9833e9674e36ba0
usmanshafique7 <121858355+usmanshafique7@users.noreply.github.com>
2024-11-21 17:01:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Not welcome to my react app hkhk
+          Not welcome to my react app
         </p>
         <a
           className="App-link"
