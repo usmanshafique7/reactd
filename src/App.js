0d2bc57197002a425c0a514365f6607da851dda7
usmanshafique7 <s7.usman@gmail.com>
2024-12-07 01:23:03 +0500

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Talha
+          I done mY Firsy Ci/CD Project
         </p>
         <a
           className="App-link"
