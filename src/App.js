670599dec471125b3b28a2512f415f77db91a59d
usmanshafique7 <s7.usman@gmail.com>
2024-12-10 17:45:01 +0500

    type mistake

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I done mY Firsy Ci/CD Project
+          I done mY FirsT Ci/CD Project
         </p>
         <a
           className="App-link"
